0a9b0f482acf125a563f35cec7956b5fbc777ce2
Michele Brambilla <michele.brambilla@psi.ch>
2017-05-19 14:52:21 +0200

    refactored control

--- neventGenerator/control.hpp
+++ neventGenerator/control.hpp
@@ -1,60 +1,49 @@
+#include <atomic>
 #include <iostream>
 #include <stdlib.h>
 
-#include "factory.hpp"
+//#include "factory.hpp"
 
 namespace control {
 
 const uint32_t default_rate = 10;
 
 struct Control {
-  int update() {
-    return stop_;
-  };
-  virtual int start(int) {
-    return stop_;
-  };
-  const uint32_t &rate() {
-    return rate_;
-  };
+  int update() { return stop_; };
+  virtual int start(int) { return stop_; };
+  const uint32_t&& rate() { return std::move(rate_.load()); };
   bool run() const { return status_ == run_; }
   bool pause() const { return status_ == pause_; }
   bool stop() const { return status_ == stop_; }
 
 protected:
-  enum {
-    run_,
-    pause_,
-    stop_
-  };
-  int status_;
-  uint32_t rate_ = default_rate;
+  enum { run_, pause_, stop_ };
+  std::atomic<int> status_;
+  std::atomic<uint32_t> rate_{default_rate};
 };
 
-using ControlFactory = factory::ObjectFactory<Control, std::string>;
+  //using ControlFactory = factory::ObjectFactory<Control, std::string>;
 
 struct NoControl : public Control {
-  NoControl() { status_ = run_; }
-  NoControl(const NoControl &) { status_ = run_; }
-  NoControl(const uparam::Param &) { status_ = run_; }
+  NoControl() { status_.store(run_); }
+  NoControl(const NoControl &) { status_.store(run_); }
+  NoControl(const uparam::Param &) { status_.store(run_); }
 
-  int update() { return status_; }
+  int&& update() { return std::move(status_.load()); }
 };
 
 struct CommandlineControl : public Control {
-  CommandlineControl() { status_ = run_; }
-  CommandlineControl(const CommandlineControl &other) {
-    status_ = other.status_;
-    rate_ = other.rate_;
-  }
+  CommandlineControl() { status_.store(run_); }
+  CommandlineControl(const CommandlineControl &other) = default;
+  //     : status_(other.status_), rate_(other.rate_) {}
   CommandlineControl(uparam::Param &p) {
-    status_ = run_;
-    rate_ = std::stoi(p["rate"]);
+    status_.store(run_);
+    rate_.store(std::stoi(p["rate"]));
   }
 
   CommandlineControl &operator=(CommandlineControl &other) {
-    status_ = other.status_;
-    rate_ = other.rate_;
+    status_.store(other.status_);
+    rate_.store(other.rate_);
     return *this;
   }
 
@@ -62,31 +51,38 @@ struct CommandlineControl : public Control {
 
   int start(int value) {
     if (value >= 0 && value < 3) {
-      status_ = value;
+      status_.store(value);
     }
     return run();
   }
 
 private:
-  int update_impl() {
+  int&& update_impl() {
+    std::cout << "run_status : " << status_ << "\t"
+              << "transmissison_rate : " << std::to_string(rate_) << "\n";
     std::string value;
     while (status_ != stop_) {
       std::cin >> value;
-      if (std::string(value) == "run" || std::string(value) == "ru")
-        status_ = run_;
-      if (std::string(value) == "pause" || std::string(value) == "pa")
-        status_ = pause_;
-      if (std::string(value) == "stop" || std::string(value) == "st")
-        status_ = stop_;
+      if (std::string(value) == "run" || std::string(value) == "ru") {
+        status_.store(run_);
+      }
+      if (std::string(value) == "pause" || std::string(value) == "pa") {
+        status_.store(pause_);
+      }
+      if (std::string(value) == "stop" || std::string(value) == "st") {
+        status_.store(stop_);
+      }
       if (std::string(value) == "rate" || std::string(value) == "ra") {
         std::cout << "Insert the new transmission rate:" << std::endl;
         std::cin >> value;
-        rate_ = std::stoi(value);
+        rate_.store(std::stoi(value));
+        std::cout << value << std::endl;
       }
-      std::cout << "status : " << status_ << std::endl;
     }
-    return status_;
+    std::cout << "status : " << status_ << "\t"
+              << "tr : " << std::to_string(rate_) << "\n";
+    return std::move(status_.load());
   }
 };
 
-} // control
+} // namespace control
