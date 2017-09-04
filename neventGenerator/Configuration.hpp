c91d7de6b33cd3bc9fe59318a096caed8a36bb44
Michele Brambilla <michele.brambilla@psi.ch>
2017-09-04 08:04:38 +0200

    read configuration file & parse uri

--- neventGenerator/Configuration.hpp
+++ neventGenerator/Configuration.hpp
@@ -2,32 +2,39 @@
 
 #include "Errors.hpp"
 
+#include "rapidjson/document.h"
+
 #include <string>
 
 namespace SINQAmorSim {
-  
-  class Configuration {
-    using Error = ConfigurationError;
-    
-  public:
-    std::string configuration_file{""};
-    std::string produce_broker{""};
-    std::string filename{""};
-    int multiplier{1};
-    int rate{0};
-  };
-  
-  class ConfigurationParser {
-    
-    int parse_configuration_file(const std::string& input);
-    int parse_command_line();
-    
-  private:
-    int parse_configuration_file_impl(rapidjson::Document& d);
-    int parse_string_uri(const std::string& uri);
-    
-    Configuration config;
-    
-  };
 
+class KafkaConfiguration {
+public:
+  std::string broker{""};
+  std::string topic{""};
+};
+
+class Configuration {
+
+public:
+  KafkaConfiguration producer;
+  std::string protocol{""};
+  std::string configuration_file{""};
+  std::string filename{""};
+  int multiplier{1};
+  int rate{0};
+};
+
+class ConfigurationParser {
+
+public:
+  int parse_configuration_file(const std::string &input);
+  int parse_command_line(int argc, char** argv);
+
+  int parse_configuration_file_impl(rapidjson::Document &d);
+  KafkaConfiguration parse_string_uri(const std::string &uri,
+                                      const bool use_defaults=false);
+
+  Configuration config;
+};
 }
--- neventGenerator/tests/parser.cxx
+++ neventGenerator/tests/parser.cxx
@@ -1,9 +1,58 @@
 #include "../Configuration.hpp"
+#include <gtest/gtest.h>
 
-
-TEST(ConfigurationParser, parse_broker_port_topic){
-  ConfigurationParser p;
-  GTEST_EXPECT(p.parse_string_uri("hello") == 0);
+auto source_dir=std::string(CMAKE_CURRENT_SOURCE_DIR);
   
-  
+TEST(ConfigurationParser, use_default_if_wrong_syntax) {
+  SINQAmorSim::ConfigurationParser parser;
+  auto configuration = parser.parse_string_uri("hello",true);
+  EXPECT_EQ(configuration.broker, "localhost:9092");
+  EXPECT_EQ(configuration.topic, "empty-topic");
 }
+
+TEST(ConfigurationParser, leave_empty_if_wrong_syntax) {
+  SINQAmorSim::ConfigurationParser parser;
+  auto configuration = parser.parse_string_uri("hello",false);
+  EXPECT_EQ(configuration.broker, "");
+  EXPECT_EQ(configuration.topic, "");
+
+  configuration = parser.parse_string_uri("//localhost:9092/",false);
+  EXPECT_EQ(configuration.broker, "localhost:9092");
+  EXPECT_EQ(configuration.topic, "");
+
+  configuration = parser.parse_string_uri("//:/my-topic",false);
+  EXPECT_EQ(configuration.broker, "");
+  EXPECT_EQ(configuration.topic, "my-topic");  
+}
+
+TEST(ConfigurationParser, parse_broker_port_topic) {
+  SINQAmorSim::ConfigurationParser parser;
+  auto configuration = parser.parse_string_uri("//localhost:8080/my-topic");
+  EXPECT_EQ(configuration.broker, "localhost:8080");
+  EXPECT_EQ(configuration.topic, "my-topic");
+}
+
+TEST(ConfigurationParser, missing_json_configuration) {
+  SINQAmorSim::ConfigurationParser parser;
+  auto filename = source_dir+"/missing_configuration.json";
+  auto result = parser.parse_configuration_file(filename);
+  EXPECT_EQ(result,SINQAmorSim::ConfigurationError::error_input_file);
+}
+
+TEST(ConfigurationParser, invalid_json_configuration) {
+  SINQAmorSim::ConfigurationParser parser;
+  
+  auto filename = source_dir+"/invalid_configuration.json";
+  auto result = parser.parse_configuration_file(filename);
+  EXPECT_NE(result,
+            SINQAmorSim::ConfigurationError::error_no_configuration_error);
+}
+
+TEST(ConfigurationParser, read_json_configuration) {
+  SINQAmorSim::ConfigurationParser parser;
+  auto filename = source_dir+"/valid_configuration.json";
+  auto result = parser.parse_configuration_file(filename);
+  EXPECT_EQ(result,
+            SINQAmorSim::ConfigurationError::error_no_configuration_error);
+}
+
