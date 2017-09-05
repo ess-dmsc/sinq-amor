dd49d97869318329625e01fcd819005e020db8ac
Michele Brambilla <michele.brambilla@psi.ch>
2017-09-05 13:08:48 +0200

    parse command line and override configuration file

---
+++ neventGenerator/Errors.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+
+namespace SINQAmorSim {
+  enum GenericError {
+    error_unknown = -1001
+  };
+
+  enum ConfigurationError {
+    error_no_configuration_error = 0,
+    error_input_file = -10,
+    error_parsing_json = -11,
+    error_parsing_command_line = -12,
+    error_configuration_invalid = -13
+  };
+
+}
