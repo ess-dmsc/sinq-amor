97dafb797b5702e8162ab1abf70df6863bbc66f2
Michele Brambilla <michele.brambilla@psi.ch>
2018-04-17 09:45:52 +0200

    Apply clang format to kafka_generator, some cleaning

--- neventGenerator/kafka_generator.hpp
+++ neventGenerator/kafka_generator.hpp
@@ -14,7 +14,7 @@
 
 namespace SINQAmorSim {
 
-uint64_t timestamp_now() {
+uint64_t getCurrentTimestamp() {
   return std::chrono::duration_cast<std::chrono::nanoseconds>(
              std::chrono::system_clock::now().time_since_epoch())
       .count();
@@ -22,25 +22,25 @@ uint64_t timestamp_now() {
 
 struct KafkaGeneratorInfo {
   double Mbytes{0};
-  double messages{0};
+  double NumMessages{0};
 };
 
 class DeliveryReport : public RdKafka::DeliveryReportCb {
 public:
-  void dr_cb(RdKafka::Message &message) {
-    if (message.errstr() == "Success") {
-      info.messages++;
-      info.Mbytes += message.len() * 1e-6;
+  void dr_cb(RdKafka::Message &Message) {
+    if (Message.errstr() == "Success") {
+      Info.NumMessages++;
+      Info.Mbytes += Message.len() * 1e-6;
     } else {
-      std::cout << message.errstr() << std::endl;
+      std::cout << Message.errstr() << std::endl;
     }
   }
 
-  double &messages() { return info.messages; }
-  double &Mbytes() { return info.Mbytes; }
+  double &getNumMessages() { return Info.NumMessages; }
+  double &getMbytes() { return Info.Mbytes; }
 
 private:
-  KafkaGeneratorInfo info;
+  KafkaGeneratorInfo Info;
 };
 
 ////////////////
@@ -49,65 +49,64 @@ private:
 template <class Serialiser> class KafkaTransmitter {
 
 public:
-  KafkaTransmitter(const std::string &brokers, const std::string &topic_str,
-                   const std::string &src_name,
-                   const KafkaOptions &options = {})
-      : topic_name{topic_str}, source_name{src_name} {
-    if (brokers.empty() || topic_name.empty()) {
+  KafkaTransmitter(const std::string &Brokers, const std::string &TopicName,
+                   const std::string &SourceName,
+                   const KafkaOptions &Options = {})
+      : Topic{TopicName}, Source{SourceName} {
+    if (Brokers.empty() || Topic.empty()) {
       throw std::runtime_error("Broker and/or topic not set");
     }
 
-    std::unique_ptr<RdKafka::Conf> conf{
+    std::unique_ptr<RdKafka::Conf> Configuration{
         RdKafka::Conf::create(RdKafka::Conf::CONF_GLOBAL)};
-    if (!conf) {
+    if (!Configuration) {
       throw std::runtime_error("Unable to create RdKafka::Conf");
     }
 
-    std::string errstr;
-    std::string debug;
-    conf->set("metadata.broker.list", brokers, errstr);
-    if (!errstr.empty()) {
-      std::cerr << errstr << std::endl;
+    std::string Error;
+    Configuration->set("Metadata.broker.list", Brokers, Error);
+    if (!Error.empty()) {
+      std::cerr << Error << std::endl;
     }
-    if (!errstr.empty()) {
-      std::cerr << errstr << std::endl;
+    if (!Error.empty()) {
+      std::cerr << Error << std::endl;
     }
-    conf->set("api.version.request", "true", errstr);
-    if (!errstr.empty()) {
-      std::cerr << errstr << std::endl;
+    Configuration->set("api.version.request", "true", Error);
+    if (!Error.empty()) {
+      std::cerr << Error << std::endl;
     }
-    conf->set("dr_cb", &dr_cb, errstr);
-    if (!errstr.empty()) {
-      std::cerr << errstr << std::endl;
+    Configuration->set("dr_cb", &DeliveryCallback, Error);
+    if (!Error.empty()) {
+      std::cerr << Error << std::endl;
     }
-    for (auto &o : options) {
-      conf->set(o.first, o.second, errstr);
-      if (!errstr.empty()) {
-        std::cerr << errstr << std::endl;
+    for (auto &Option : Options) {
+      Configuration->set(Option.first, Option.second, Error);
+      if (!Error.empty()) {
+        std::cerr << Error << std::endl;
       }
     }
 
-    producer.reset(RdKafka::Producer::create(conf.get(), errstr));
-    if (!producer) {
-      throw std::runtime_error("Failed to create producer: " + errstr);
+    Producer.reset(RdKafka::Producer::create(Configuration.get(), Error));
+    if (!Producer) {
+      throw std::runtime_error("Failed to create producer: " + Error);
     }
 
     RdKafka::Metadata *md;
-    auto err = producer->metadata(1, nullptr, &md, 1000);
-    if (err == RdKafka::ERR__TIMED_OUT) {
-      throw std::runtime_error("Failed to retrieve metadata: " + errstr);
+    auto Err = Producer->metadata(1, nullptr, &md, 1000);
+    if (Err == RdKafka::ERR__TIMED_OUT) {
+      throw std::runtime_error("Failed to retrieve Metadata: " + Error);
     }
-    metadata.reset(md);
+    Metadata.reset(md);
 
-    serialiser.reset(new Serialiser{source_name});
+    SerialiserWorker.reset(new Serialiser{Source});
   }
 
-  template <typename T> size_t send(std::vector<T> &data, const int nev = -1) {
-    RdKafka::ErrorCode resp = producer->produce(
-        topic_name, RdKafka::Topic::PARTITION_UA,
-        RdKafka::Producer::RK_MSG_COPY, &data[0], data.size() * sizeof(T),
-        nullptr, 0, timestamp_now(), nullptr);
-    return int(producer->poll(10));
+  template <typename T> size_t send(std::vector<T> &Data, const int nev = -1) {
+    RdKafka::ErrorCode resp = Producer->produce(
+        Topic, RdKafka::Topic::PARTITION_UA, RdKafka::Producer::RK_MSG_COPY,
+        &Data[0], Data.size() * sizeof(T), nullptr, 0, getCurrentTimestamp(),
+        nullptr);
+    return int(Producer->poll(10));
   }
 
   template <typename T>
@@ -116,36 +115,33 @@ public:
     return 0;
   }
 
-  int poll(const int &seconds = -1) { return producer->poll(seconds); }
+  int poll(const int &Seconds = -1) { return Producer->poll(Seconds); }
 
-  double &messages() { return dr_cb.messages(); }
-  double &Mbytes() { return dr_cb.Mbytes(); }
+  double &getNumMessages() { return DeliveryCallback.getNumMessages(); }
+  double &getMbytes() { return DeliveryCallback.getMbytes(); }
 
 private:
-  std::unique_ptr<RdKafka::Metadata> metadata{nullptr};
-  std::unique_ptr<RdKafka::Producer> producer{nullptr};
-  std::string topic_name;
-  std::string source_name;
+  std::unique_ptr<RdKafka::Metadata> Metadata{nullptr};
+  std::unique_ptr<RdKafka::Producer> Producer{nullptr};
+  std::string Topic;
+  std::string Source;
 
-  DeliveryReport dr_cb;
-  std::unique_ptr<Serialiser> serialiser{nullptr};
+  DeliveryReport DeliveryCallback;
+  std::unique_ptr<Serialiser> SerialiserWorker{nullptr};
 };
-
-template <typename T> class TD;
 
 template <>
 template <typename T>
 size_t KafkaTransmitter<FlatBufferSerialiser>::send(
-    const uint64_t &pid, const std::chrono::nanoseconds &pulse_time,
-    std::vector<T> &data, const int nev) {
-  if (nev) {
-    serialiser->serialise(pid, pulse_time, data);
-    RdKafka::ErrorCode resp =
-        producer->produce(topic_name, RdKafka::Topic::PARTITION_UA,
-                          RdKafka::Producer::RK_MSG_COPY,
-                          reinterpret_cast<void *>(serialiser->get()),
-                          serialiser->size(), nullptr, 0, // timestamp_now()
-                          pulse_time.count(), nullptr);
+    const uint64_t &PacketID, const std::chrono::nanoseconds &PulseTime,
+    std::vector<T> &Events, const int NumEvents) {
+  if (NumEvents) {
+    SerialiserWorker->serialise(PacketID, PulseTime, Events);
+    RdKafka::ErrorCode resp = Producer->produce(
+        Topic, RdKafka::Topic::PARTITION_UA, RdKafka::Producer::RK_MSG_COPY,
+        reinterpret_cast<void *>(SerialiserWorker->get()),
+        SerialiserWorker->size(), nullptr, 0, // timestamp_now()
+        PulseTime.count(), nullptr);
   }
   return 0;
 }
@@ -154,48 +150,47 @@ size_t KafkaTransmitter<FlatBufferSerialiser>::send(
 // Consumer
 
 template <class Serialiser> struct KafkaListener {
-  static const int max_header_size = 10000;
 
-  KafkaListener(const std::string &brokers, const std::string &topic_str,
-                const std::string &src_name, const KafkaOptions &options)
-      : source_name{src_name} {
-    std::unique_ptr<RdKafka::Conf> conf{
+  KafkaListener(const std::string &Brokers, const std::string &TopicName,
+                const std::string &SourceName, const KafkaOptions &Options)
+      : Source{SourceName} {
+    std::unique_ptr<RdKafka::Conf> Configuration{
         RdKafka::Conf::create(RdKafka::Conf::CONF_GLOBAL)};
-    std::unique_ptr<RdKafka::Conf> tconf{
+    std::unique_ptr<RdKafka::Conf> TopicConfiguration{
         RdKafka::Conf::create(RdKafka::Conf::CONF_TOPIC)};
 
-    std::string errstr;
-    conf->set("metadata.broker.list", brokers, errstr);
-    if (!errstr.empty()) {
-      std::cerr << errstr << std::endl;
+    std::string Error;
+    Configuration->set("metadata.broker.list", Brokers, Error);
+    if (!Error.empty()) {
+      std::cerr << Error << std::endl;
     }
-    for (auto &o : options) {
-      conf->set(o.first, o.second, errstr);
-      if (!errstr.empty()) {
-        std::cerr << errstr << std::endl;
+    for (auto &Option : Options) {
+      Configuration->set(Option.first, Option.second, Error);
+      if (!Error.empty()) {
+        std::cerr << Error << std::endl;
       }
     }
 
-    if (topic_str.empty()) {
+    if (TopicName.empty()) {
       std::cerr << "Topic required." << std::endl;
       exit(1);
     }
-    consumer.reset(RdKafka::Consumer::create(conf.get(), errstr));
-    if (!consumer) {
-      std::cerr << "Failed to create consumer: " << errstr << std::endl;
+    Consumer.reset(RdKafka::Consumer::create(Configuration.get(), Error));
+    if (!Consumer) {
+      std::cerr << "Failed to create consumer: " << Error << std::endl;
       exit(1);
     }
-    topic.reset(
-        RdKafka::Topic::create(consumer.get(), topic_str, tconf.get(), errstr));
-    if (!topic) {
-      std::cerr << "Failed to create topic: " << errstr << std::endl;
+    Topic.reset(RdKafka::Topic::create(Consumer.get(), TopicName,
+                                       TopicConfiguration.get(), Error));
+    if (!Topic) {
+      std::cerr << "Failed to create topic: " << Error << std::endl;
       exit(1);
     }
 
-    RdKafka::ErrorCode resp =
-        consumer->start(topic.get(), partition, start_offset);
-    if (resp != RdKafka::ERR_NO_ERROR) {
-      std::cerr << "Failed to start consumer: " << RdKafka::err2str(resp)
+    RdKafka::ErrorCode ErrorCode =
+        Consumer->start(Topic.get(), Partition, StartOffset);
+    if (ErrorCode != RdKafka::ERR_NO_ERROR) {
+      std::cerr << "Failed to start consumer: " << RdKafka::err2str(ErrorCode)
                 << std::endl;
       exit(1);
     }
@@ -207,46 +202,43 @@ template <class Serialiser> struct KafkaListener {
   }
 
 private:
-  int32_t partition = 0;
-  int64_t start_offset = RdKafka::Topic::OFFSET_END;
-  std::string source_name;
+  int32_t Partition = 0;
+  int64_t StartOffset = RdKafka::Topic::OFFSET_END;
+  std::string Source;
 
-  std::unique_ptr<RdKafka::Consumer> consumer{nullptr};
-  std::unique_ptr<RdKafka::Topic> topic{nullptr};
+  std::unique_ptr<RdKafka::Consumer> Consumer{nullptr};
+  std::unique_ptr<RdKafka::Topic> Topic{nullptr};
 };
 
 template <>
 template <typename T>
 std::pair<uint64_t, uint64_t>
-KafkaListener<FlatBufferSerialiser>::recv(std::vector<T> &data) {
+KafkaListener<FlatBufferSerialiser>::recv(std::vector<T> &Events) {
 
-  std::pair<int, int> result;
-  std::unique_ptr<RdKafka::Message> msg{nullptr};
-  auto rcv_stat = RdKafka::ERR_NO_ERROR;
-  FlatBufferSerialiser s;
+  std::unique_ptr<RdKafka::Message> Message{nullptr};
+  FlatBufferSerialiser SerialiserWorker;
 
   do {
-    msg.reset(consumer->consume(topic.get(), partition, 1000));
-    rcv_stat = msg->err();
-    if ((rcv_stat != RdKafka::ERR_NO_ERROR) &&
-        (rcv_stat != RdKafka::ERR__TIMED_OUT) &&
-        (rcv_stat != RdKafka::ERR__PARTITION_EOF)) {
-      std::cerr << "message error: " << RdKafka::err2str(msg->err())
+    Message.reset(Consumer->consume(Topic.get(), Partition, 1000));
+    if ((Message->err() != RdKafka::ERR_NO_ERROR) &&
+        (Message->err() != RdKafka::ERR__TIMED_OUT) &&
+        (Message->err() != RdKafka::ERR__PARTITION_EOF)) {
+      std::cerr << "message error: " << RdKafka::err2str(Message->err())
                 << std::endl;
     }
-  } while (msg->err() != RdKafka::ERR_NO_ERROR);
+  } while (Message->err() != RdKafka::ERR_NO_ERROR);
 
-  uint64_t pid = -1;
-  std::chrono::nanoseconds pulse_time{0};
-  std::string source;
-  s.extract(reinterpret_cast<const char *>(msg->payload()), data, pid,
-            pulse_time, source);
-  if (!source_name.empty()) {
-    if (source_name != source) {
+  uint64_t MessageID = -1;
+  std::chrono::nanoseconds PulseTime{0};
+  std::string MessageSource;
+  SerialiserWorker.extract(reinterpret_cast<const char *>(Message->payload()),
+                           Events, MessageID, PulseTime, MessageSource);
+  if (!Source.empty()) {
+    if (Source != MessageSource) {
       return std::pair<uint64_t, uint64_t>{0, 0};
     }
   }
-  return std::pair<uint64_t, uint64_t>{pid, msg->len()};
+  return std::pair<uint64_t, uint64_t>{MessageID, Message->len()};
 }
 
 } // namespace SINQAmorSim
