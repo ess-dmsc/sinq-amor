9303b4a35f345674c7458d8c91b3563224bbeaf7
Michele Brambilla <michele.brambilla@psi.ch>
2018-05-02 10:25:37 +0200

    Add jsonformoderncpp dependencies

---
+++ neventGenerator/json.h
@@ -0,0 +1,27 @@
+#include <nlohmann/json.hpp>
+
+#include <string>
+
+template <typename T> class JsonMaybe {
+public:
+  JsonMaybe() {}
+  JsonMaybe(T inner) : inner_(inner), found_(true) {}
+  explicit operator bool() const { return found_; }
+  T inner() const { return inner_; }
+
+private:
+  T inner_;
+  bool found_ = false;
+};
+
+template <typename T>
+JsonMaybe<T> find(std::string Key, nlohmann::json const &Json);
+
+template <typename T>
+JsonMaybe<T> find(std::string Key, nlohmann::json const &Json) {
+  auto It = Json.find(Key);
+  if (It != Json.end()) {
+    return JsonMaybe<T>(It.value().get<T>());
+  }
+  return JsonMaybe<T>();
+}
