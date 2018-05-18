213094d903c9c4a8c016ba78b01084791b89e013
Michele Brambilla <michele.brambilla@psi.ch>
2018-05-18 11:56:06 +0200

    Reduce statistics among threads

---
+++ neventGenerator/Stats.cxx
@@ -0,0 +1,52 @@
+#include "Stats.hpp"
+#include <iostream>
+#include <numeric>
+
+Stats::~Stats() { Run = false; }
+uint64_t Stats::getCurrentTimestamp() {
+  return std::chrono::duration_cast<std::chrono::nanoseconds>(
+             std::chrono::system_clock::now().time_since_epoch())
+      .count();
+}
+
+void Stats::setNumThreads(const int NumThreads) {
+  NumMessages.resize(NumThreads);
+  MBytes.resize(NumThreads);
+}
+
+void Stats::add(const int Messages, const int MB, const int ThreadId) {
+  NumMessages[ThreadId] += Messages;
+  MBytes[ThreadId] += MB;
+  std::lock_guard<std::mutex> Lock(CountGuard);
+  ThreadCount++;
+  if (ThreadCount == NumMessages.size()) {
+    WaitUntilReady.notify_all();
+    ThreadCount = 0;
+  }
+}
+void Stats::report() {
+
+  while (Run) {
+    std::chrono::time_point<std::chrono::system_clock> StartTime =
+        system_clock::now();
+    std::unique_lock<std::mutex> Lock(CountGuard);
+    WaitUntilReady.wait(Lock);
+    std::chrono::time_point<std::chrono::system_clock> Now =
+        system_clock::now();
+
+    int Messages = std::accumulate(NumMessages.begin(), NumMessages.end(), 0);
+    int MB = std::accumulate(MBytes.begin(), MBytes.end(), 0);
+
+    std::fill(NumMessages.begin(), NumMessages.end(), 0);
+    std::fill(MBytes.begin(), MBytes.end(), 0);
+
+    std::cout << "Sent " << Messages << " packets @ "
+              << 1e3 * MB /
+                     std::chrono::duration_cast<std::chrono::milliseconds>(
+                         Now - StartTime)
+                         .count()
+              << "MB/s"
+              << "\t(timestamp : " << getCurrentTimestamp() << ")" << std::endl;
+    StartTime = Now;
+  }
+}
---
+++ neventGenerator/Stats.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
+#include <vector>
+
+struct Stats {
+
+  using system_clock = std::chrono::system_clock;
+
+public:
+  ~Stats();
+
+  void setNumThreads(const int);
+  void add(const int, const int, const int);
+  void report();
+
+private:
+  uint64_t getCurrentTimestamp();
+
+  std::vector<int> NumMessages;
+  std::vector<int> MBytes;
+  std::mutex CountGuard;
+  std::condition_variable WaitUntilReady;
+  std::atomic<int> ThreadCount{0};
+  bool Ready{false};
+  bool Run{true};
+};
