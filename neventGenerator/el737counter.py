56b7b7f0261d1836abdd1b1580568caf27345659
Michele Brambilla <michele.brambilla@psi.ch>
2018-11-30 16:14:34 +0100

    minor changes to the EL737counter simulation

--- neventGenerator/el737counter.py
+++ neventGenerator/el737counter.py
@@ -1,22 +1,23 @@
-#!/usr/bin/python -p
+#!/usr/bin/python
 #
 # fake SINQ EL737 counter box
 #
 # I am using 1000cts/sec for m1, 500 for m2, 300 for m3 and 2000 for m4
 #
 # Mark Koennecke, July 2015
-#----------------------------------------------------------------------
-from twisted.internet import reactor, protocol
-from twisted.protocols.basic import LineReceiver
-import time
-import sys
-import subprocess as sp
+# ----------------------------------------------------------------------
 import os
+import sys
+import time
+import logging
 
 import generator
+from twisted.internet import reactor, protocol
+from twisted.protocols.basic import LineReceiver
 
 
 class EL737Controller(LineReceiver):
+
     def __init__(self):
         self.remotestate = 0
         self.delimiter = '\r'
@@ -32,19 +33,26 @@ class EL737Controller(LineReceiver):
         self.thresholdcounter = 1
         self.proc = MyPIPE()
         self.generator = generator.Generator()
+        self.generator_path = None
+
+    def set_path(self, path):
+        self.generator_path = path
 
     def write(self, data):
-        print "transmitted:", data
+        logging.info("transmitted: %s", data)
         if self.transport is not None:
             self.transport.write(data)
 
     def to_process(self, data):
-        print "transmitted to process:", data
-        if self.transport is not None:
-            self.proc.transport.write(data)
+        logging.info("transmitted to process:", data)
+        try:
+            if self.transport is not None:
+                self.proc.transport.write(data.encode())
+        except Exception as e:
+            logging.error(e, exc_info=True)
 
     def calculateCountStatus(self):
-        if self.counting and not self.mypaused :
+        if self.counting and not self.mypaused:
             runtime = time.time() - self.starttime - self.pausedTime
             print(str(runtime) + ' versus ' + str(self.preset))
             if self.mode == 'timer':
@@ -53,13 +61,13 @@ class EL737Controller(LineReceiver):
                     self.endtime = self.starttime + self.preset
                     self.pausedTime = 0
             else:
-                if runtime*1000 >= self.preset:
+                if runtime * 1000 >= self.preset:
                     self.counting = False
-                    self.endtime = self.starttime + self.preset/1000
-        print('count flag after calculateCountStatus ' + str(self.counting))
+                    self.endtime = self.starttime + self.preset / 1000
+        logging.debug('count flag after calculateCountStatus %s' + str(self.counting))
 
     def lineReceived(self, data):
-        print "lineReceived:", data
+        logging.info("lineReceived: %s", data)
 
         orig = data.strip()
         data = data.lower().strip()
@@ -76,13 +84,12 @@ class EL737Controller(LineReceiver):
         if self.remotestate == 1:
             if data.startswith('echo 2'):
 
-                g.find('./build')
-                print orig.split()[2:]
-#                if not g.validate(orig.split()[2:]):
-#                    self.remotestate = 1
-#                    return
-
-                reactor.spawnProcess(self.proc,g.exe[0],args=orig.split()[2:],env=os.environ)
+                try:
+                    g.find(self.generator_path)
+                    reactor.spawnProcess(self.proc, g.exe, args=orig.split()[2:], env=os.environ)
+                except Exception as what:
+                    logging.error(what, exc_info=True)
+                    self.remotestate == 0
 
                 self.remotestate = 2
                 self.write("\r")
@@ -92,175 +99,169 @@ class EL737Controller(LineReceiver):
 
         if self.remotestate == 2:
 
-           if data.startswith('rmt 1') or data.startswith('echo'):
-               self.write('\r')
-               return
+            if data.startswith('rmt 1') or data.startswith('echo'):
+                self.write('\r')
+                return
+            arguments = data.split()
 
-           if data.startswith('mp'):
-               l = data.split()
-               if len(l) < 2:
-                   self.write("argument required\r")
-                   return
-               self.mode = 'monitor'
-               self.preset = float(l[1])
-               self.starttime = time.time()
-               self.mypaused = False
-               self.pausedTime = 0.
-               self.counting = True
-               self.write('\r')
-               self.to_process('run\r')
-               return
+            if data.startswith('mp'):
+                if len(arguments) < 2:
+                    self.write("argument required\r")
+                    return
+                self.mode = 'monitor'
+                self.preset = float(arguments[1])
+                self.starttime = time.time()
+                self.mypaused = False
+                self.pausedTime = 0.
+                self.counting = True
+                self.write('\r')
+                self.to_process('run\r')
+                return
 
-           if data.startswith('tp'):
-               l = data.split()
-               if len(l) < 2:
-                   self.write("argument required\r")
-                   return
-               self.mode = 'timer'
-               self.preset = float(l[1])
-               self.starttime = time.time()
-               self.mypaused = False
-               self.pausedTime = 0.
-               self.counting = True
-               self.write('\r')
-               self.to_process('run\r')
-               return
+            if data.startswith('tp'):
+                if len(arguments) < 2:
+                    self.write("argument required\r")
+                    return
+                self.mode = 'timer'
+                self.preset = float(arguments[1])
+                self.starttime = time.time()
+                self.mypaused = False
+                self.pausedTime = 0.
+                self.counting = True
+                self.write('\r')
+                self.to_process('run\r')
+                return
 
-           if data.startswith('st'):
-               self.counting = False
-               self.endtime = time.time()
-               self.write('\r')
-               self.to_process('stop\r')
-               return
+            if data.startswith('st'):
+                self.counting = False
+                self.endtime = time.time()
+                self.write('\r')
+                self.to_process('stop\r')
+                return
 
-           if data.startswith('ps'):
-               self.counting = True
-               self.mypaused = True
-               self.pausestart = time.time()
-               self.write('\r')
-               self.to_process('pause\r')
-               return
+            if data.startswith('ps'):
+                self.counting = True
+                self.mypaused = True
+                self.pausestart = time.time()
+                self.write('\r')
+                self.to_process('pause\r')
+                return
 
-           if data.startswith('rt'):
-               l = data.split()
-               if len(l) < 2:
-                   self.write("argument required\r")
-                   return
-               self.to_process('rate\r')
-               self.to_process(l[1])
-               return
+            if data.startswith('rt'):
+                if len(arguments) < 2:
+                    self.write("argument required\r")
+                    return
+                self.to_process('rate\r')
+                self.to_process(arguments[1])
+                return
 
-           if data.startswith('co'):
-               if not self.mypaused:
-                   pass
-               else:
-                   self.mypaused = False
-                   self.pausedTime  += time.time() - self.pausestart
-               self.to_process('run\r')
-               self.write('\r')
-               return
+            if data.startswith('co'):
+                if not self.mypaused:
+                    pass
+                else:
+                    self.mypaused = False
+                    self.pausedTime += time.time() - self.pausestart
+                self.to_process('run\r')
+                self.write('\r')
+                return
 
-           if data.startswith('dl'):
-               l = data.split()
-               if len(l) < 2:
-                   self.write("argument required\r")
-                   return
-               if len(l) >= 3:
-                   self.threshold = float(l[2])
-                   self.write('\r')
-               else:
-                   self.write(str(self.threshold) + '\r')
+            if data.startswith('dl'):
+                if len(arguments) < 2:
+                    self.write("argument required\r")
+                    return
+                if len(arguments) >= 3:
+                    self.threshold = float(arguments[2])
+                    self.write('\r')
+                else:
+                    self.write(str(self.threshold) + '\r')
 
-           if data.startswith('dr'):
-               l = data.split()
-               if len(l) >= 2:
-                   self.thresholdcounter = int(l[1])
-                   self.write('\r')
-               else:
-                   self.write(str(self.thresholdcounter) + '\r')
+            if data.startswith('dr'):
+                if len(arguments) >= 2:
+                    self.thresholdcounter = int(arguments[1])
+                    self.write('\r')
+                else:
+                    self.write(str(self.thresholdcounter) + '\r')
 
-           if data.startswith('rs'):
-               self.calculateCountStatus()
-               if self.counting:
-                   if self.mypaused:
-                       if self.mode == 'timer':
-                           self.write('9\r')
-                       else:
-                           self.write('10\r')
-                   else:
-                       if self.mode == 'timer':
-                           self.write('1\r')
-                       else:
-                           self.write('2\r')
-               else:
-                   self.write('0\r')
-               return
+            if data.startswith('rs'):
+                self.calculateCountStatus()
+                if self.counting:
+                    if self.mypaused:
+                        if self.mode == 'timer':
+                            self.write('9\r')
+                        else:
+                            self.write('10\r')
+                    else:
+                        if self.mode == 'timer':
+                            self.write('1\r')
+                        else:
+                            self.write('2\r')
+                else:
+                    self.write('0\r')
+                return
 
-           if data.startswith('ra'):
-               self.calculateCountStatus()
-               if self.counting:
-                   if self.mypaused:
-                       pausetime = time.time() - self.pausestart
-                   else:
-                       pausetime = self.pausedTime
-                   diff = time.time() - self.starttime - pausetime
-               else:
-                   diff = self.endtime - self.starttime
-               rlist = []
-               rlist.append(str(diff))
-               rlist.append(str(int(diff*1000)))
-               rlist.append(str(int(diff*1500)))
-               rlist.append(str(int(diff*500)))
-               rlist.append(str(int(diff*300)))
-               rlist.append(str(int(diff*2000)))
-               rlist.append('0')
-               rlist.append('0')
-               rlist.append('0')
-               rastring = ' '.join(rlist)
-               self.write(rastring +'\r')
-               return
+            if data.startswith('ra'):
+                self.calculateCountStatus()
+                if self.counting:
+                    if self.mypaused:
+                        pausetime = time.time() - self.pausestart
+                    else:
+                        pausetime = self.pausedTime
+                    diff = time.time() - self.starttime - pausetime
+                else:
+                    diff = self.endtime - self.starttime
+                rlist = List()
+                rlist.append(str(diff))
+                rlist.append(str(int(diff * 1000)))
+                rlist.append(str(int(diff * 1500)))
+                rlist.append(str(int(diff * 500)))
+                rlist.append(str(int(diff * 300)))
+                rlist.append(str(int(diff * 2000)))
+                rlist.append('0')
+                rlist.append('0')
+                rlist.append('0')
+                rastring = ' '.join(rlist)
+                self.write(rastring + '\r')
+                return
 
-           if data.startswith('id'):
-               self.write('EL737 Neutron Counter V8.02\r')
-               return
+            if data.startswith('id'):
+                self.write('EL737 Neutron Counter V8.02\r')
+                return
 
-           self.write('?2\r')
+            self.write('?2\r')
 
     def connectionLost(self, reason):
         self.to_process('exit\r')
         time.sleep(1)
         self.proc.transport.signalProcess('KILL')
-        print "Goodbye..."
-        print reason
+        logging.info("Goodbye... %s",reason)
 
 
 class MyPIPE(protocol.ProcessProtocol):
+
     def connectionMade(self):
-        print "  Connection made"
+        logging.info("  Connection made")
 
     def childDataReceived(self, childFD, data):
-        print "Message from ",childFD,":"
-        print data
+        print("Message from %s", childFD, ":")
+        print(data)
 
     def connectionLost(self, reason):
-        print "Goodbye..."
+        logging.info("Goodbye...")
 
-    def outReceived(self,data):
-        print "out : ",data
+    def outReceived(self, data):
+        print("out : ", data)
 
-    def errReceived(self,data):
-        print "err : ",data
+    def errReceived(self, data):
+        print("err : ", data)
 
-    def processExited(self,reason):
-        if reason.value.exitCode != 0 :
-            print "exit : ",reason.value
-        else :
-            print "exit"
+    def processExited(self, reason):
+        if reason.value.exitCode != 0:
+            print("exit : ", reason.value)
+        else:
+            print("exit")
 
-    def processEnded(self,reason):
-        print "process ended : ",reason.value
-        print "quitting"
-
+    def processEnded(self, reason):
+        logging.info("process ended : ", reason.value)
 
 
 def main(argv):
@@ -271,8 +272,12 @@ def main(argv):
 
     factory = protocol.ServerFactory()
     factory.protocol = EL737Controller
+    if len(argv) > 2:
+        factory.protocol.generator_path = argv[2]
+
     reactor.listenTCP(port, factory)
     reactor.run()
+
 
 if __name__ == "__main__":
     main(sys.argv)
--- neventGenerator/generator.py
+++ neventGenerator/generator.py
@@ -1,45 +1,49 @@
-import os, fnmatch
+import fnmatch
+import os
+
 
 class Generator:
-
     # This list has to be in the favorite order: if multiple
     # executables are find the first will be used
-    default_paths = [ '.',
-                      './build/',
-                      '/opt/amor/simfiles/',
-                      '/opt/software/sinq-amorsim/',
-                      '/opt/amor/simfiles/']
-    exe = [];
+    exe = None
 
-    def find_impl(self,pattern, path):
-        result = []
+    @staticmethod
+    def find_impl(pattern, path):
         for root, dirs, files in os.walk(path):
             for name in files:
                 if fnmatch.fnmatch(name, pattern):
-                    result.append(os.path.join(root, name))
-        return result
+                    return os.path.join(root, name)
+        return None
 
-    def find(self,path):
-        exe = self.find_impl('AMORgenerator', path)
-        if exe != []:
-            self.exe = exe
-            return
-        for i in self.default_paths:
-            exe = self.find_impl('AMORgenerator', i)
-            if exe != []:
+    def find(self, path=None):
+        paths = ['./build/',
+                 '.',
+                 '/opt/amor/simfiles/',
+                 '/opt/software/sinq-amorsim/',
+                 '/opt/amor/simfiles/']
+        if path is not None:
+            paths.insert(0, path)
+
+        for p in paths:
+            exe = self.find_impl('AMORgenerator', p)
+            if exe:
                 self.exe = exe
-                return
+                return exe
+        raise Exception("Can't find event generator in selected paths. Abort.")
 
-    def validate(self, param) :
+    @staticmethod
+    def validate(param):
         pos = [i for i, j in enumerate(param) if j == '-b']
-        if pos != []:
-            return True
-        return False
+        if not pos:
+            return False
+        return True
 
-def main() :
+
+def main():
     g = Generator()
     g.find()
-    print g.exe
+    print(g.exe)
+
 
 if __name__ == "__main__":
     main()
